0058911fa559b9b10cc1bacbb5edb512e814cebf
raluka <badoi.raluca@gmail.com>
2016-04-26 16:59:06 +0200

    Move Class Methods to IdeaStore

--- app.rb
+++ app.rb
@@ -1,4 +1,5 @@
 require './idea'
+require './idea_store'
 
 class IdeaBoxApp < Sinatra::Base
   configure :development do
@@ -12,29 +13,28 @@ class IdeaBoxApp < Sinatra::Base
   end
 
   get '/' do
-    erb :index, locals: { ideas: Idea.all }
+    erb :index, locals: { ideas: IdeaStore.all, idea: Idea.new(params) }
   end
 
   # Create new idea
   post '/' do
-    idea = Idea.new(params[:idea])
-    idea.save
+    idea = IdeaStore.create(params[:idea])
     redirect '/'
   end
 
   delete '/:id' do |id|
-    Idea.delete(id.to_i)
+    IdeaStore.delete(id.to_i)
     redirect('/')
   end
 
   get '/:id/edit' do |id|
-    idea = Idea.find(id.to_i)
+    idea = IdeaStore.find(id.to_i)
     erb :edit, locals: { id: id, idea: idea }
   end
 
   # Update idea
   put '/:id' do |id|
-    Idea.update(id.to_i, params[:idea])
+    IdeaStore.update(id.to_i, params[:idea])
     redirect'/'
   end
 end
