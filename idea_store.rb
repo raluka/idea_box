77d795c09deadcec1acbdf12a0105a4d27f7e247
raluca <raluka@users.noreply.github.com>
2016-04-26 17:00:36 +0200

    Move Class Methods to IdeaStore (#7)

---
+++ idea_store.rb
@@ -0,0 +1,51 @@
+require 'yaml/store'
+
+class IdeaStore
+
+  class << self
+    def database
+      @database ||= YAML::Store.new "ideabox"
+    end
+
+    def raw_ideas
+      database.transaction do |db|
+        db['ideas'] || []
+      end
+    end
+
+    def create(attributes)
+      database.transaction do
+        database['ideas'] ||= []
+        database['ideas'] << attributes
+      end
+    end
+
+    def all
+      raw_ideas.map do |data|
+        Idea.new(data)
+      end
+    end
+
+    def find_raw_idea(id)
+      database.transaction do
+        database['ideas'].at(id)
+      end
+    end
+
+    def find(id)
+      Idea.new(find_raw_idea(id))
+    end
+
+    def update(id, data)
+      database.transaction do
+        database['ideas'][id] = data
+      end
+    end
+
+    def delete(position)
+      database.transaction do
+        database['ideas'].delete_at(position)
+      end
+    end
+  end
+end
